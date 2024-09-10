9ca45f90ad3d78d1e3ec22f3e23e3080d3462eb5
AMIR REYES <amir.reyes@utp.ac.pa>
2024-09-10 16:59:17 -0500

    Laboratorio 4 Completo

---
+++ Laboratorio4/Laboratorio4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int fact = 1, n;
+        string linea;
+        Console.Write("Ingrese un numero entero: ");
+        linea = Console.ReadLine(); 
+        n = int.Parse(linea);
+        for (int i = 1; i <= n; i++)
+        {
+            fact = fact * i;
+        }
+        Console.Write("La factorial es: " + fact);
+        Console.ReadKey();
+    }
+}
