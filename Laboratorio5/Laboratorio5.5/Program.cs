5677a7faab65ef201463c269885b9e26570e690e
Reyes-25 <amir.reyesb@gmail.com>
2024-09-11 20:30:50 -0500

    Laboratorio 5 Completo

---
+++ Laboratorio5/Laboratorio5.5/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Estudiante
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+    public void Mostrar()
+    {
+        List<Estudiante> estudiantes = new List<Estudiante>
+{
+        new Estudiante { Nombre = "Ana", Edad = 12},
+        new Estudiante { Nombre = "Juan", Edad = 10 },
+        new Estudiante { Nombre = "Sofia", Edad = 11}
+};
+
+        foreach (Estudiante estudiante in estudiantes)
+        {
+            Console.WriteLine("Nombre: " + estudiante.Nombre + ", Edad: " + estudiante.Edad);
+        }
+
+    }
+
+
+    private static void Main(string[] args)
+    {
+        Estudiante estudiante = new Estudiante();
+        estudiante.Mostrar();
+    }
+}
