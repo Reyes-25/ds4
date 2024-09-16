0ca8557016d64de51400a95a5e2d84160ae5671a
Reyes-25 <amir.reyesb@gmail.com>
2024-09-16 03:05:47 -0500

    Laboratorio 6 Completo

---
+++ Laboratorio6/Laboratorio6.3/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            int[] myNumbers = { 1, 2, 3 };
+            Console.WriteLine(myNumbers[10]);
+        }catch (Exception e)
+        {
+            Console.WriteLine("Algo salio mal, válide el dice del arreglo");
+        }
+        finally
+        {
+            Console.WriteLine("Continuación de la aplicación, luego del bloque try/catch");
+        }  
+    }
+}
