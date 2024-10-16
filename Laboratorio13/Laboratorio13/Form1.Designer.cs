72807a9096b71b959a46eb29d529752b74668db8
Reyes25 <amir.reyesb@gmail.com>
2024-10-16 00:28:14 -0500

    Laboratorio 13 Completo

---
+++ Laboratorio13/Laboratorio13/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Laboratorio13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_conectar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(95, 136);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(134, 147);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btn_conectar
+            // 
+            this.btn_conectar.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btn_conectar.Location = new System.Drawing.Point(95, 72);
+            this.btn_conectar.Name = "btn_conectar";
+            this.btn_conectar.Size = new System.Drawing.Size(134, 45);
+            this.btn_conectar.TabIndex = 1;
+            this.btn_conectar.Text = "Conectar y Desconectar al SQL Server";
+            this.btn_conectar.UseVisualStyleBackColor = true;
+            this.btn_conectar.Click += new System.EventHandler(this.btn_conectar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 450);
+            this.Controls.Add(this.btn_conectar);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_conectar;
+    }
+}
+
---
+++ Laboratorio13/Laboratorio13/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Laboratorio13
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=.\sqlexpress;Database=Northwind;TrustServerCertificate=true;Integrated Security=SSPI;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_conectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(connectionString))
+                {
+                    conexion.Open();
+                    MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos.");
+                    LoadProductName();
+                    conexion.Close();
+                    MessageBox.Show("Se cerró la conexión.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void LoadProductName()
+        {
+            String sql = "Select ProductName from Products";
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        listBox1.Items.Clear();
+                        while (reader.Read())
+                        {
+                            listBox1.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("Error al cargar los productos: ", e.Message);
+            }
+        }
+    }
+}
