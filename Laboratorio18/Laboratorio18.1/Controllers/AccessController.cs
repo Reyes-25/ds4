5559044b16d96676f3094496b8558b2088b966dd
Reyes25 <amir.reyesb@gmail.com>
2024-11-17 11:58:13 -0500

    Laboratorio 18 Completo

---
+++ Laboratorio18/Laboratorio18.1/Controllers/AccessController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio18._1.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(String user, string password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch(Exception ex)
+            {
+                return Content("Ocurrió un error: (" + ex.Message);
+            }
+        }
+    }
+}
---
+++ Laboratorio18/Laboratorio18.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laboratorio18._1.MvcApplication" Language="C#" %>
